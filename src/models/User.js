848aa3f34f4716938d98af86664da896e163f164
KOSASIH <kosasihg88@gmail.com>
2024-11-04 10:24:53 +0700

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+// src/models/User.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 30,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function(v) {
+                return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// Hash the password before saving
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Export the User model
+module.exports = mongoose.model('User ', userSchema);
